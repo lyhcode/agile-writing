08e0cc6e5fb9ea1881830a4b8f0ec8786f5c50af
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-09-21 17:41:26 +0800

    change make.sh

--- make.sh
+++ make.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # make clean
+make epub
 make latex
+
 cd _build/latex
+xelatex *.tex
 xelatex *.tex
 
 cd ..
 cd ..
 
-cp -f _build/latex/*.pdf free
+cp -f _build/latex/*.pdf free/
+cp -f _build/epub/*.epub free/
 
 open free/*.pdf
 
+make clean
